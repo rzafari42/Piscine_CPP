15bf76dd790aed5b794ba5ceeba93631aae0658e
Redwane ZAFARI <rzafari@e1r6p13.42.fr>
2021-06-28 12:31:27 +0200

    C01/ex06 started

--- C01/ex05/Karen.hpp
+++ C01/ex05/Karen.hpp
@@ -15,5 +15,18 @@
 # include <iostream>
 # include <string>
 
+class Karen
+{
+    private:
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
 
+    public:
+        Karen();
+        ~Karen();
+
+        void complain( std::string level );
+};
 #endif
