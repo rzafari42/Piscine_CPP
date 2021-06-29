d45539404ae31c5ad5a39797b8833b5f44364f66
Redwane ZAFARI <redwanezafari@MacBook-Pro-de-Redwane.local>
2021-06-29 16:24:54 +0200

    C01/ex03 pointer initialized in HumanB.cpp

--- C01/ex03/HumanB.cpp
+++ C01/ex03/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 12:42:58 by rzafari           #+#    #+#             */
-/*   Updated: 2021/06/27 22:58:20 by rzafari          ###   ########.fr       */
+/*   Updated: 2021/06/29 16:23:54 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB( std::string name ) : _name(name)
+HumanB::HumanB( std::string name ) : _name(name), _weapon(NULL)
 {
 }
 
