a6daa17050db8f2673e686b751904a453f143801
Redwane ZAFARI <rzafari@e1r8p19.42.fr>
2021-07-02 14:40:34 +0200

    C04. Main ex00 done

--- C04/ex00/main.cpp
+++ C04/ex00/main.cpp
@@ -6,25 +6,27 @@
 /*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 10:23:10 by rzafari           #+#    #+#             */
-/*   Updated: 2021/07/02 12:39:41 by rzafari          ###   ########.fr       */
+/*   Updated: 2021/07/02 14:35:47 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Sorcerer.hpp"
 #include "Victim.hpp"
 #include "Peon.hpp"
-#include <fstream>
+#include "More.hpp"
 
-int main()
+int main(void)
 {
     Sorcerer robert("Robert", "the Magnificent");
     Victim jim("Jimmy");
     Peon joe("Joe");
+    More lola("lola");
     
-    std::cout << robert << jim << joe;
+    std::cout << robert << jim << joe << lola;
 
     robert.polymorph(jim);
     robert.polymorph(joe);
+    robert.polymorph(lola);
 
     return 0;
 }
