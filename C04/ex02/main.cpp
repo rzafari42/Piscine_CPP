88dc930c5e223e7cbea09a9dcc9456b08fc43b27
Redwane ZAFARI <rzafari@e1r10p18.42.fr>
2021-07-05 14:19:10 +0200

    C04/ex02. General main test adde

--- C04/ex02/main.cpp
+++ C04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 16:38:19 by rzafari           #+#    #+#             */
-/*   Updated: 2021/07/05 14:16:13 by rzafari          ###   ########.fr       */
+/*   Updated: 2021/07/05 14:18:29 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,3 +44,24 @@ int main( void )
 
     return 0;
 }
+
+
+/*int main()
+{
+    ISpaceMarine* bob = new TacticalMarine;
+    ISpaceMarine* jim = new AssaultTerminator;
+
+    ISquad* vlc = new Squad;
+    vlc->push(bob);
+    vlc->push(jim);
+    for (int i = 0; i < vlc->getCount(); ++i)
+    {
+        ISpaceMarine* cur = vlc->getUnit(i);
+        cur->battleCry();
+        cur->rangedAttack();
+        cur->meleeAttack();
+    }
+    delete vlc;
+
+    return 0;
+}*/
