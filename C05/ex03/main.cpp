d3d57809d9641b07beb27cbb8eb3706aed25af94
Redwane ZAFARI <rzafari@e1r12p19.42.fr>
2021-07-06 17:30:49 +0200

    C05 ex03, main improved

--- C05/ex03/main.cpp
+++ C05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 15:37:58 by rzafari           #+#    #+#             */
-/*   Updated: 2021/06/14 16:45:28 by rzafari          ###   ########.fr       */
+/*   Updated: 2021/07/06 17:20:07 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,77 +19,9 @@
 
 int		main(void)
 {
-	Bureaucrat	supreme("Supreme Bob", 1);
-	Bureaucrat	officer("Officer Bob", 140);
-	Bureaucrat	justBob("Bob the peasant", 150);
-	ShrubberyCreationForm	F182("target_tree");
-	RobotomyRequestForm		C182("target_robot");
-	PresidentialPardonForm	B182("target_pardon");
-
-	std::cout << "\t\t###############################################" << std::endl;
-	std::cout << supreme << officer << justBob;
-	std::cout << "\t\t###############################################" << std::endl;
-	std::cout << F182;
-	justBob.signForm(F182);
-	std::cout << F182;
-	officer.executeForm(F182);
-	std::cout << F182;
-	officer.signForm(F182);
-	std::cout << F182;
-	officer.executeForm(F182);
-	supreme.executeForm(F182);
-	std::cout << "\t\t###############################################" << std::endl;
-	std::cout << (officer = Bureaucrat("", 60));
-	std::cout << "\t\t###############################################" << std::endl;
-	std::cout << C182;
-	justBob.signForm(C182);
-	std::cout << C182;
-	officer.executeForm(C182);
-	std::cout << C182;
-	officer.signForm(C182);
-	std::cout << C182;
-	officer.executeForm(C182);
-	supreme.executeForm(C182);
-	supreme.executeForm(C182);
-	supreme.executeForm(C182);
-	supreme.executeForm(C182);
-	std::cout << "\t\t###############################################" << std::endl;
-	std::cout << (officer = Bureaucrat("", 10));
-	std::cout << "\t\t###############################################" << std::endl;
-	std::cout << B182;
-	justBob.signForm(B182);
-	std::cout << B182;
-	officer.executeForm(B182);
-	std::cout << B182;
-	officer.signForm(B182);
-	std::cout << B182;
-	officer.executeForm(B182);
-	supreme.executeForm(B182);
-	std::cout << "\t\t###############################################" << std::endl;
-
-	Intern  someRandomIntern;
-	Form*   rrf;
-
-	std::cout << officer;
-	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
-	std::cout << *rrf;
-	officer.executeForm(*rrf);
-	officer.signForm(*rrf);
-	officer.executeForm(*rrf);
-	std::cout << "\t\t###############################################" << std::endl;
-	rrf = someRandomIntern.makeForm("shrubbery creation", "John");
-	std::cout << *rrf;
-	officer.executeForm(*rrf);
-	officer.signForm(*rrf);
-	officer.executeForm(*rrf);
-	std::cout << "\t\t###############################################" << std::endl;
-	rrf = someRandomIntern.makeForm("presidential pardon", "Bobby");
-	std::cout << *rrf;
-	officer.executeForm(*rrf);
-	officer.signForm(*rrf);
-	officer.executeForm(*rrf);
-	std::cout << "\t\t###############################################" << std::endl;
-	rrf = someRandomIntern.makeForm("presidential pard", "Bobby");
-
+	Intern someRandomIntern;
+	
+	Form* rrf;
+	rrf = someRandomIntern.makeForm("shrubbery creation", "Bender");
 	return (0);
 }
