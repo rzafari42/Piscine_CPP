d2ef7050210d024397f80278799fd2859ee99012
rzafari42 <redwane.zafari@hotmail.com>
2021-07-07 00:08:04 +0200

    C05 -> need to check memory leaks before pushing

--- C05/ex03/main.cpp
+++ C05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 15:37:58 by rzafari           #+#    #+#             */
-/*   Updated: 2021/07/06 17:20:07 by rzafari          ###   ########.fr       */
+/*   Updated: 2021/07/07 00:03:29 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,22 @@
 int		main(void)
 {
 	Intern someRandomIntern;
-	
-	Form* rrf;
+	Form*		rrf = NULL;
+	Form*		fail = NULL;
+
 	rrf = someRandomIntern.makeForm("shrubbery creation", "Bender");
+
+	Bureaucrat b0("b0", 42);
+
+	if (rrf != NULL)
+	{	
+		std::cout << *rrf << std::endl;
+		b0.signForm(*rrf);
+		b0.executeForm(*rrf);
+		delete rrf;
+	}
+
+	fail = someRandomIntern.makeForm("qzdsqdsd", "intern");
+
 	return (0);
 }
