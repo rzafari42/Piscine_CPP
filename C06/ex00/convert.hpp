ddc813754c51de5455ffbba23af9dd738bfa68d8
Redwane ZAFARI <rzafari@e1r10p19.42.fr>
2021-07-08 10:59:08 +0200

    New subjects upload

--- C06/ex00/convert.hpp
+++ C06/ex00/convert.hpp
@@ -6,7 +6,7 @@
 /*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 11:21:10 by rzafari           #+#    #+#             */
-/*   Updated: 2021/06/17 10:07:50 by rzafari          ###   ########.fr       */
+/*   Updated: 2021/07/07 12:11:04 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <stdlib.h>
 
 int     precision(char *input);
-void        convert_char(double arg);
+void        convert_char(double arg, char *input);
 void        convert_int(double arg);
 void        convert_float(double arg, int prec);
 void        convert_double(double arg, int prec);
