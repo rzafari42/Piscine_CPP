0459af57b2017f0ec465ccb6149c61c5ee11ac40
Redwane ZAFARI <rzafari@e2r7p12.42.fr>
2021-07-13 11:01:50 +0200

    C07

--- C07/ex00/whatever.hpp
+++ C07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/20 12:57:25 by rzafari           #+#    #+#             */
-/*   Updated: 2021/07/13 10:17:20 by rzafari          ###   ########.fr       */
+/*   Updated: 2021/07/13 11:00:29 by rzafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,13 @@
 
 class Awesome {
 	public:
-		Awesome( int n ) : _n( n ) {}
+		Awesome( void ) {return;};
+	
+    	Awesome( int n ) : _n( n ) {}
+        Awesome( Awesome const& src ) { *this = src; return;}
+        ~Awesome() { return; }
+        Awesome & operator=( Awesome const& rhs ) { if (this != &rhs) { this->_n = rhs._n; } return *this; };
+
 		bool operator==( Awesome const & rhs ) { return (this->_n == rhs._n); }
 		bool operator!=( Awesome const & rhs ) { return (this->_n != rhs._n); }
 		bool operator>( Awesome const & rhs ) { return (this->_n > rhs._n); }
@@ -31,14 +37,15 @@ class Awesome {
 	private:
 		int _n;
 };
-std::ostream & operator<<(std::ostream &flux, const Awesome &src) { flux << src.get_n(); return (flux); }
+
+std::ostream & operator<<(std::ostream &flux, const Awesome &src) { flux << src.get_n(); return flux; }
 
 template<typename T>
 void swap(T &v1, T &v2)
 {
-    T v3(v1);
+    T v3;
 
-    //v3 = v1;
+    v3 = v1;
     v1 = v2;
     v2 = v3;
 }
@@ -50,7 +57,7 @@ T const min(T &v1, T &v2)
 }
 
 template<typename T> 
-T const max(T v1, T v2)
+T const max(T &v1, T &v2)
 {
     return ((v1 > v2) ? v1 : v2);
 }
